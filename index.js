7388aff52a5b95e7cc2fdd56ca3aeb9eb2235381
Matt Steiner <Metropolis.4488@gmail.com>
2020-10-05 10:59:44 -0400

    update

--- index.js
+++ index.js
@@ -1,16 +1,57 @@
 // array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-];
 
-// function to write README file
-function writeToFile(fileName, data) {
+fileCreator()
+
+async function fileCreator() {
+    const {title, description, instructions} = await inquirer.prompt([
+    {
+        question: "What is the project title?",
+        response: "title"
+    },
+    {
+        question: "Provided a description of the project",
+        response: "description"
+    },
+    {
+        question: "Do you have installation instructions?",
+        response: "instructions"
+    },
+    {
+        question: ,
+        response:
+    },
+    {
+        question: ,
+        response:
+    },
+    {
+        question: ,
+        response:
+    },
+    ])
 }
 
-// function to initialize program
-function init() {
 
-}
 
-// function call to initialize program
-init();
+
+
+
+
+// const questions = [
+
+// ];
+
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
+
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
